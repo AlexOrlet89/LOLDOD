af657e1d86432871a709e3bfd377e4b7cd970397
Alex Orlet <AlexOrlet89@gmail.com>
2022-06-22 15:43:59 -0700

    create new user test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,13 +3,37 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const mockUser = {
+  email: 'test@dod.com',
+  password: '12345',
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+
+  const agent = request.agent(app);
+
+  const user = await UserService.create({ ...mockUser, ...userProps });
+
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
+};
+
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('creates a new user', async () => {
+    const res = await (await request(app).post('/api/v1/users')).send(mockUser);
+    const { email } = mockUser;
+
+    expect(res.body).toEqual({
+      email,
+    });
   });
+
   afterAll(() => {
     pool.end();
   });
