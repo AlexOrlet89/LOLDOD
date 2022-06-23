ed942611788f68d2238e4bb3cbea714e91b48484
Alex Orlet <AlexOrlet89@gmail.com>
2022-06-23 15:38:11 -0700

    started logout testing, realized my login testing needs authentication

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,7 +8,7 @@ const mockUser = {
   password: '12345',
 };
 
-describe('backend-express-template routes', () => {
+describe('user testing', () => {
   beforeEach(() => {
     return setup(pool);
   });
@@ -23,14 +23,28 @@ describe('backend-express-template routes', () => {
     });
   });
   it('signs in an existing user', async () => {
+    const { email } = mockUser;
+
     await request(app).post('/api/v1/users').send(mockUser);
     const res = await request(app)
       .post('/api/v1/users/sessions')
       .send({ email: 'test@dod.com', password: '12345' });
     expect(res.status).toEqual(200);
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email,
+    });
   });
+  it.skip('logs out a user', async () => {
+    await request(app).post('/api/v1/users').send(mockUser);
+    const res = await request(app)
+      .post('/api/v1/users/sessions')
+      .send({ email: 'test@dod.com', password: '12345' });
+    expect(res.status).toEqual(200);
+    await request(app).delete('/api/v1/users/sessions');
 
-  afterAll(() => {
-    pool.end();
+    afterAll(() => {
+      pool.end();
+    });
   });
 });
