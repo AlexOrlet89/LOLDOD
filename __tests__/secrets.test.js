9124e844b197071359c8e742f302e90eca3c72e8
Alex Orlet <AlexOrlet89@gmail.com>
2022-06-23 17:40:27 -0700

    secrets revealed test written, fails with expected 401 message

--- __tests__/secrets.test.js
+++ __tests__/secrets.test.js
@@ -4,16 +4,30 @@ const request = require('supertest');
 
 const pool = require('../lib/utils/pool');
 
+const mockUser = {
+  email: 'test@dod.com',
+  password: '12345',
+};
+
 describe('user testing', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('should return a 401 when returning state secrets', async () => {
+  it.skip('should return a 401 when returning state secrets', async () => {
     const res = await request(app).get('/api/v1/secrets');
     expect(res.body).toEqual({
       message: 'You must be signed in to continue',
       status: 401,
     });
+  });
+  it('should return a list of secrets if user is signed in', async () => {
+    await request(app).post('/api/v1/users').send(mockUser);
+    const res = await request(app)
+      .post('/api/v1/users/sessions')
+      .send({ email: 'test@dod.com', password: '12345' });
+    expect(res.status).toEqual(200);
+    const secretsRevealed = await request(app).get('/api/v1/secrets');
+    expect(secretsRevealed.status).toEqual(200);
   });
   afterAll(() => {
     pool.end();
